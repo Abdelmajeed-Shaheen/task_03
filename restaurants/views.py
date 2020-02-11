5afcbc7613f5b9ce5d6dfb416c9bde1afc47ce43
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-02-11 20:04:42 +0200

    test_passed

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,33 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+            "my_list":[
+                    {
+                            "restaurant_name":"WINGERS",
+                            "food_type":"Burgers & Wings",
+                            "opens_at":"12PM",
+                            "closes_at":"12AM",
+                    },
+                    {
+                            "restaurant_name":"MAC",
+                            "food_type":"Fast Food",
+                            "opens_at":"4am",
+                            "closes_at":"--",
+                    },
+                    {
+                            "restaurant_name":"BK",
+                            "food_type":"Burgers",
+                            "opens_at":"12PM",
+                            "closes_at":"03AM",
+                    },
+                    {
+                            "restaurant_name":"KFC",
+                            "food_type":"Burgers & Wings",
+                            "opens_at":"01PM",
+                            "closes_at":"12AM",
+                    },
 
+            ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +40,13 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+        "my_object": {
+                "title":"WINGERS",
+                "restaurant_name":"WINGERS",
+                "food_type":"Burgers & Wings",
+                "opens_at":"12PM",
+                "closes_at":"12AM",
+        },
 
     }
     return render(request, 'detail.html', context)
